9f098c10b0e21720a6bf83696141951f6c743f51
Jordan A. Holt <jordanh316@gmail.com>
2026-03-01 22:58:54 +0000

    Add shared audit log service and wire QuickBooks + rental application mutations

--- tenant_portal_backend/src/rental-application/rental-application.module.ts
+++ tenant_portal_backend/src/rental-application/rental-application.module.ts
@@ -10,11 +10,12 @@ import { NotificationsModule } from '../notifications/notifications.module';
 import { OptionalJwtAuthGuard } from '../auth/optional-jwt.guard';
 import { OrgContextGuard } from '../common/org-context/org-context.guard';
 import { RentalApplicationAiService } from './rental-application.ai.service';
+import { AuditLogService } from '../shared/audit-log.service';
 
 @Module({
   imports: [PrismaModule, SecurityEventsModule, NotificationsModule, HttpModule],
   controllers: [RentalApplicationController],
-  providers: [RentalApplicationService, ApplicationLifecycleService, OptionalJwtAuthGuard, OrgContextGuard, RentalApplicationAiService],
+  providers: [RentalApplicationService, ApplicationLifecycleService, OptionalJwtAuthGuard, OrgContextGuard, RentalApplicationAiService, AuditLogService],
   exports: [RentalApplicationService, ApplicationLifecycleService],
 })
 export class RentalApplicationModule {}
